70de4d2291a85cd6cd4e9afca9c7b554043f57c8
Kairo Ribeiro <kairo10@gmail.com>
2022-07-26 17:47:31 -0400

    fix

--- models/note.js
+++ models/note.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 const noteSchema = new mongoose.Schema({
   title: String,
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
-  date: Date,
   note: String,
 },{
   timestamps: true,
